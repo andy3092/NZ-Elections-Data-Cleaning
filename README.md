65d1f2cc88bdfa54d0fbad725070cc40f3e908c4
Andrew Rae <andrew.rae@vuw.ac.nz>
2015-07-27 15:59:49 +1200

    Cleaned up the functions for creating the quries to write data to the sqlite database.

--- README.md
+++ README.md
@@ -4,7 +4,20 @@ Some scripts to download and clean New Zealand election results data and load th
 At the moment downloads and cleans up the 2014 election party vote results.
 
 # TODO
-Add some proper commandline arguments for the scrips instead of hardcoding options in. e.g. year you want to download and clean, where you want to save files, output file names etc.
+Add some proper commandline arguments for the scrips 
+instead of hardcoding options in. 
+e.g. year you want to download and clean, where you want to save files, 
+output file names etc.
+
+Split off the Maori electrates into a seprate table
+
+Add other parties to the dictionary from other years for renaming the columns
+
+Fix up the download script
+
+
+
+
 
 
 
--- load2sqlite.py
+++ load2sqlite.py
@@ -1,20 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python2
 
 from pandas import DataFrame, Series
 import pandas as pd
 import numpy as np
 import os
-from sqlalchemy import create_engine
+#from sqlalchemy import create_engine
 import sqlite3
+#import ipdb
 
-"""
-Script to clean and load election data in an sqlite database
-"""
+'''
+Script to clean and load election data to an sqlite database
+From csv files.
+'''
 
-
+#-----------------------------------------------------------------
 # Constants
 # Dictionary for renaming columns for setting up the schemea
+# Removes MAcrons and spaces never good as field names
+# for a database.
+#-----------------------------------------------------------------
 HEADER_DICT = {u'Unnamed: 0': u'Suburb',
                u'Unnamed: 1': u'Voting_Place', 
                u'ACT New Zealand': u'ACT',
@@ -26,8 +30,7 @@ HEADER_DICT = {u'Unnamed: 0': u'Suburb',
                u'Green Party': u'Green_Party',
                u'Internet MANA': u'Internet_Mana',
                u'Labour Party': u'Labour',
-               #u'M\xc4\x81ori Party': u'Maori_Party',
-               u'Māori Party': u'Maori_Party',
+               u'M\u0101ori Party': u'Maori_Party',
                u'National Party': u'National',
                u'New Zealand First Party': u'NZ_First',
                u'NZ Independent Coalition': u'NZ_Independant',
@@ -36,85 +39,110 @@ HEADER_DICT = {u'Unnamed: 0': u'Suburb',
                u'Total Valid Party Votes': u'Valid',
                u'Informal Party Votes': u'Informal'}
 
-#def load_csv(filename):
-#    df = pd.read_csv('csv/e9_part8_party_13.csv', skiprows=2)
-    
-
-def changeencode(data, cols):
-    '''
-    Small function to encode Unicode caracters. 
-    '''
-    for col in cols:
-        data[col] = data[col].str.encode('utf-8')
-    return data 
-
 def load_csv (file_name):
-    #file_name = 'csv/e9_part8_party_18.csv'
+    '''
+    Loads and cleans csv files into a pandas data frame
+    Ready for writing out to an sqlite database.
+    '''
+    #-----------------------------------------------------------------
     # Get the Electorate Number
+    #-----------------------------------------------------------------
     electorate_num = int(file_name.split('_')[-1].rstrip('.csv'))
 
+    #-----------------------------------------------------------------
     # Skip first two rows and the last 18 lines of the file
+    #-----------------------------------------------------------------
     df = pd.read_csv(file_name, skiprows=2, skipfooter=18, encoding='UTF-8')
 
-    # Rename the columns with the HEADER_DICT
-    # Clean out any macrons
-    df = df.rename(columns=HEADER_DICT)
+    #-----------------------------------------------------------------
+    # Rename the columns
+    # Strip out macrons and spaces as these are no good as field names
+    #-----------------------------------------------------------------
+    df = df.rename(columns=HEADER_DICT)     
 
+    #-----------------------------------------------------------------
     # drop the row 'Voting places where less than 6 votes were taken'
+    #-----------------------------------------------------------------
     filter = 'Voting places where less than 6 votes were taken'
     df = df[df['Voting_Place'] != filter]
-    
+
+    #-----------------------------------------------------------------
     # Add  a column with the electrate number
-    df['ElectID'] = electorate_num
+    #-----------------------------------------------------------------
+    df['ElectID'] = electorate_num     
 
     # forward fill the first column 
     # exclude the last 5 rows as it does not apply
     last5rows = len(df) - 5
     df.loc[:last5rows, 'Suburb'] = df['Suburb'].fillna(method='ffill')
 
-    # Reoder the columns so ElecID is the first coulmn
+    # The Maori electorates have rows that just have a surburb name in them
+    # and null values for the votes
+    df = df.dropna(thresh=10) 
+
+    #-----------------------------------------------------------------
+    # Reorder the columns so ElecID is the first coulmn
+    #-----------------------------------------------------------------
     cols = df.columns.tolist()
     cols = cols[-1:] + cols[:-1]
     return df[cols]
 
-def create_party_table(con, table_name, columns):
+def create_table_query(table_name, columns):
     '''
-    Takes a list of parties and 
-    creates an sql table
-    returns a connection to the table.
+    Takes a list of columns of votes adds a primary key.
+    Returns an sql query string 
     '''
     int_cols = ', '.join([col + ' INTEGER' for col in columns])
 
-    query = """
+    return  """
     CREATE TABLE IF NOT EXISTS """ + table_name + """
-    (ID INTEGER, ElectID INTEGER, Suburb VARCHAR(50), 
+    (ID INTEGER PRIMARY KEY, ElectID INTEGER, Suburb VARCHAR(50), 
     Voting_Place VARCHAR(255), """ + int_cols + """
     );
     """
-    con.execute(query)
-    con.commit()
 
-def write_to_sql(con, table_name, df):
-    output = df.itertuples()
-    data = tuple(output)
-    wildcards = ','.join(['?'] * (len(df.columns) + 1))
-    insert_sql = 'INSERT INTO %s VALUES (%s)' % (table_name, wildcards)
-    con.executemany(insert_sql, data)
-    con.commit()
+def create_insert_query(table_name, df):
+    '''
+    Returns the query string for inserting the data into sqlite
+    '''
+
+    columns_to_insert = ','.join(df.columns)
+    wildcards = ','.join(['?'] * (len(df.columns)))
+    return 'INSERT INTO %s (%s) VALUES (%s);' \
+                 % (table_name, columns_to_insert, wildcards)
 
 if __name__ == '__main__':
     dir = 'csv'
     filter = '.csv'
     csv_files = [os.path.join(dir, f) for f in os.listdir(dir) if filter in f]
     appended_data = []
+
     for csv_file in csv_files:
         data = load_csv(csv_file)
         appended_data.append(data)
-    results = pd.concat(appended_data)
-    
-    engine = create_engine('sqlite:///my_db.sqlite')
-    results.to_sql('party', engine, if_exists='replace', index_label='id')
-    engine.dispose() # do not want to rely on the garbage disposal.
 
+    results = pd.concat(appended_data)
+
+    # Do not need a new index dropped as will insert a primary index in the 
+    # sql table
+    #results.reset_index(inplace=True) # Reset the index
+    #results.drop('index', axis=1, inplace=True) # Drop the old index column
+
+    # Orginally used sqlalchemy but no problems with primary keys
+    #engine = create_engine('sqlite:///elections2014.sqlite')
+    #results.to_sql('party', engine, if_exists='replace', index_label='id')
+    #engine.dispose() # do not want to rely on the garbage coolection.
     
-        
+    with sqlite3.connect("test14.sqlite") as con:
+        query = create_table_query('party', results.columns[3:])
+        con.execute(query)
+        con.commit()
+
+        # Prepear the data from the dataframe
+        output = results.itertuples(index=False)
+        data = tuple(output)
+
+        # Write it to the database
+        con.executemany(create_insert_query('party', results), data)
+        con.commit()
+
